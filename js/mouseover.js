4cc56f110e6adf67792b444ad0587b606b4d39ff
gotsanity <gotsanity@gmail.com>
2014-05-24 10:05:25 -0700

    started development of a visual editor button. current progress mouseover working

---
+++ js/mouseover.js
@@ -0,0 +1,18 @@
+(function() {
+    tinymce.create('tinymce.plugins.nrdbmouseover', {
+        init : function(ed, url) {
+            ed.addButton('nrdbmouseover', {
+                title : 'Add Nrdb-lookup Mouseover',
+                image : url+'/mouseover.png',
+                onclick : function() {
+                     ed.selection.setContent('[nrdb]' + ed.selection.getContent() + '[/nrdb]');
+ 
+                }
+            });
+        },
+        createControl : function(n, cm) {
+            return null;
+        },
+    });
+    tinymce.PluginManager.add('nrdbmouseover', tinymce.plugins.nrdbmouseover);
+})();
